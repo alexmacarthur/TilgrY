c234066a1ae7cb04e09bb73a181d318acbabea45
alexmacarthur <alex@macarthur.me>
2015-04-22 23:14:18 -0500

    Added logo to login screen.

--- functions.php
+++ functions.php
@@ -16,4 +16,29 @@
         remove_meta_box( 'pageparentdiv', 'page', 'normal');
 	}
 	add_action('admin_init', 'remove_dashboard_meta');
+
+	function my_login_logo() { ?>
+	
+	    <style type="text/css">
+	        body.login div#login h1 a {
+	            background-image: url('http://tilgry.org/wp-content/themes/tilgry-theme/img/logo.svg')!important;
+	            padding-bottom: 30px;
+				width: 82%;
+				background-size: contain;
+	        }
+			#loginform #wp-submit{
+				background: rgb(63, 63, 63);
+				border-color: black;
+				-webkit-box-shadow: inset 0 1px 0 rgba(203, 203, 203, 0.5),0 1px 0 rgba(0,0,0,.15);
+				box-shadow: inset 0 1px 0 rgba(231, 231, 231, 0.5),0 1px 0 rgba(0,0,0,.15);
+			}
+			 .login input:focus{
+				border-color: rgba(169, 169, 169, 0.6);
+				-webkit-box-shadow: 0 0 2px rgba(205, 205, 205, 0.8);
+				box-shadow: 0 0 2px rgba(226, 226, 226, 0.8);
+			}
+	    </style>
+	
+	<?php }
+	add_action( 'login_enqueue_scripts', 'my_login_logo' );
 ?>
